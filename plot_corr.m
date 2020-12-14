dc06c923efe9fd448eeb702d0c48be890e851e57
jianyuTANG <jianyutang99@gmail.com>
2020-12-14 21:25:35 +0800

    plot

---
+++ plot_corr.m
@@ -0,0 +1,22 @@
+function start_pos = plot_corr(signal, preamble_signal, preamble_length, t1, t2)
+    %用定义好的带通滤波器对data进行滤波
+    hd = design(fdesign.bandpass('N,F3dB1,F3dB2',6, 2500, 5500, 48000),'butter');
+    signal = filter(hd, signal);
+    
+    signal_length = length(signal);
+    i = 1;
+    start_pos = -1;
+    corrs  = [];
+    while i < signal_length - preamble_length
+        corr = corrcoef(preamble_signal, signal(i : i+preamble_length-1));
+        corr = abs(corr(1,2));
+        corrs = [corrs corr];
+        i = i + 1;
+    end
+    figure;
+    plot(corrs);
+    hold on;
+    xline(t1);
+    xline(t2);
+    hold off;
+end

49af7b067ef38bcd81bc31d7e8e56573229b6df1
Xu Yihao <48053143+Yihao-Xu@users.noreply.github.com>
2020-12-15 19:39:06 +0800

    增加了chirp信号的快捷更改

--- playchirp.m
+++ playchirp.m
@@ -1,8 +1,8 @@
-function playchirp(f0,f1,T)
+function playchirp(f0,f1,T,method)
 Fs = 48000;
 
 t = 0:1/Fs:T;
-signal = chirp(t, f0, T, f1);
+signal = chirp(t, f0, T, f1,method);
 
 sound(signal, Fs);
 end
